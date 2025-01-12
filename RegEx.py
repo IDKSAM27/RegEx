e96a5a529380fe94462e150a11bc2c8e80885802
Sampreet Patil <sampreetpatil270@gmail.com>
2025-01-12 21:31:15 +0530

    Styling of the lang selector window.

--- RegEx.py
+++ RegEx.py
@@ -103,7 +103,7 @@ language_label.pack(pady=10)
 languages = ["C++", "Python"]
 for lang in languages:
     rb = tk.Radiobutton(language_selector, text=lang, value=lang, variable=selected_language, indicatoron=0)
-    rb.pack(anchor="w", padx=20)
+    rb.pack(anchor="w", padx=20, fill="x")
 
 continue_button = tk.Button(language_selector, text="Continue", command=open_main_window)
 continue_button.pack(pady=10)
