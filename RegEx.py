9dee8e74a77d555ac0ff35bb7a1f9b96f5430fa7
Sampreet Patil <sampreetpatil270@gmail.com>
2025-01-22 21:22:56 +0530

    Obj changed, highlighting js errors will never work, prob added at the last of the code.

--- RegEx.py
+++ RegEx.py
@@ -195,4 +195,4 @@ language_selector.mainloop()
 
 # Use the same RE technique in python too.
 # Commenting after any {} in cpp will cause missing semicolon error
-# Highlighting problem is js analysis
+# Highlighting problem is js analysis (nothing works bcs, highlighting def taked line "number" and js errors does not give line numbers in their error)
